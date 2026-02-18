0c88aeee53970633546d443bb04f13ccb41bfc19
lopodo <lopodo@yumly.es>
2026-02-18 02:18:47 +0100

    rss

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import prerender from "vite-plugin-prerender";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    prerender({
+      // Rutas que quieres prerenderizar
+      routes: ["/", "/sesion", "/servicios", "/soluciones", "/contact", "/avisolegal", "/terminos"]
+    })
+  ]
+});
