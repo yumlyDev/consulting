f33755aa389d27d478f3ce01993107639a63af21
lopodo <lopodo@yumly.es>
2026-07-26 17:55:20 +0200

    CRM

--- app/sesion/page.tsx
+++ app/sesion/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+export const dynamic = "force-dynamic";
 import React, { useState } from "react";
 import { supabase } from "@/lib/supabase";
 import { useRouter } from "next/navigation";
